5512ca1f29144547e0717f2c96af80f37fad795d
JapanSG <thanakrit.sri45326@gmail.com>
2025-11-20 12:02:02 +0000

    Automated commit from cron

--- 234/japan_script.sh
+++ 234/japan_script.sh
@@ -88,4 +88,3 @@ git diff-index --quiet HEAD || git commit -m "Automated commit from cron"
 
 # Push to GitHub using the PAT for authentication
 git push "https://${GITHUB_USERNAME}:${GITHUB_PAT}@github.com/Harley2zazaa/mss2025-project-template.git" JapanSG
-echo "https://${GITHUB_USERNAME}:${GITHUB_PAT}@github.com/Harley2zazaa/mss2025-project-template.git"
