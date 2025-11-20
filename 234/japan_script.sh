c9f232bb75e7db31b5815d670437b9c8e8aabaae
JapanSG <thanakrit.sri45326@gmail.com>
2025-11-20 13:44:35 +0000

    Automated commit from cron 234 time:2025-11-20 13:44:35

--- 234/japan_script.sh
+++ 234/japan_script.sh
@@ -92,7 +92,7 @@ echo "
   <div class="tree" style="width: auto\; max-width: 90vw\; overflow-x: auto\; padding: 10px\;">
     <div class="label">Home Directory Tree</div>
     <div class="value" style="font-size: 0.8em\; text-align: left\;">
-     <pre> $HOMEDIR </pre>
+      <div><pre> $HOMEDIR </div></pre>
     </div>
   </div>
 </div>
